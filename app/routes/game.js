c27f34317c3b586523c25c9646b1a78da18a83da
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-08-12 11:11:48 -0500

    can generate random songs on game screen. currently will do duplicates

--- app/routes/game.js
+++ app/routes/game.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 
+
 router.get('/game', function(req, res) {
-  res.render('game');
+
+  var dataFile = req.app.get('appData');
+  var song = dataFile.songs[req.params.songid];
+  var songArray = [];
+
+  function randomSong() {
+    var result = Math.floor(Math.random() * 8) + 1;
+    return result;
+  }
+
+
+  var song1 = randomSong();
+  var song2 = randomSong();
+  var song3 = randomSong();
+  var song4 = randomSong();
+
+
+  res.render('game', {
+    song1: dataFile.songs[song1].title + "    by    " + dataFile.songs[song1].artist,
+    song2: dataFile.songs[song2].title + "    by    " + dataFile.songs[song2].artist,
+    song3: dataFile.songs[song3].title + "    by    " + dataFile.songs[song3].artist,
+    song4: dataFile.songs[song4].title + "    by    " + dataFile.songs[song4].artist
+  });
 });
 
 
