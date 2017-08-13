2b681dbddd74571a47ca7354de9b8e2b3c3fb5df
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-08-12 21:26:37 -0500

    clean up api route

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
 var songData = require('../data/songFile.json');
-
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
-
 
 
 router.get('/api', function(req, res) {
   res.json(songData);
 });
 
-
-
 router.get('/api/:songId', function(req, res) {
   var chosen = req.params.songId;
 
   res.json(songData.songs[chosen]);
 });
-
 
 
 module.exports = router;
