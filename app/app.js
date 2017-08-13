d766b7ed20cf06b8d8e1ca6d00e4836f1877cd6c
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-08-13 09:41:12 -0500

    added comments to app.js

--- app/app.js
+++ app/app.js
@@ -1,17 +1,19 @@
-// This is the primary application file
+// ----------------   THIS IS THE PRIMARY APPLICATION FILE   ----------------
 
+// ----------------   Dependencies   ----------------
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
 var dataFile = require('./data/songFile.json');
 var exphbs = require("express-handlebars");
 var moment = require('moment');
-var io = require('socket.io')();
+var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 
 
-
+// ----------------   Setup Handlebars   ----------------
 app.engine("handlebars", exphbs({
   defaultLayout  : 'main',
   layoutsDir     : 'app/views/layouts/',
@@ -20,12 +22,23 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 
+// ----------------   Setup port   ----------------
 app.set('port', process.env.PORT || 3000);
+
+
+// ----------------   Setup song file at app/data/songFile   ----------------
 app.set('appData', dataFile);
+
+
+// ----------------   Setup default views folder   ----------------
 app.set('views', 'app/views');
 
 
+// ----------------   Setup public folder   ----------------
 app.use(express.static('app/public'));
+
+
+// ----------------   Setup routes   ----------------
 app.use(require('./routes/api'));
 app.use(require('./routes/category'));
 app.use(require('./routes/chat'));
@@ -36,6 +49,7 @@ app.use(require('./routes/register'));
 app.use(require('./routes/room'));
 
 
+// ----------------   Setup bodyParser   ----------------
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -44,12 +58,14 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 
-
+// ----------------   HTTP SERVER   ----------------
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
 
 
+
+// ----------------   SOCKET.IO SERVER   ----------------
 io.attach(server);
 
 io.on('connection', function(socket) {
