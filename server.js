97477dd5dd627b98dbae221d9c1b69e3577858e9
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-08-14 11:09:33 -0500

    added back missing console.log to socket server

--- server.js
+++ server.js
@@ -70,10 +70,12 @@ io.attach(server);
 
 io.on('connection', function(socket) {
   console.log('User Connected');
+  console.log(socket.id);
 
   // ----------------   Listens for messages that have been posted and resends them to all users  ----------------
   socket.on('postMessage', function(data) {
     io.emit('updateMessages', data);
+    console.log(socket.id, data);
   });
 
   socket.on('disconnect', function() {
